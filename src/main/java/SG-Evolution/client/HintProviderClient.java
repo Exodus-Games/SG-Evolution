3d829be558f1617050bb6210382c2fc00803aae5
DrakeBD <DrakeBD@users.noreply.github.com>
2018-11-29 19:57:03 +0000

    Update HintProviderClient.java

--- src/main/java/SG-Evolution/client/HintProviderClient.java
+++ src/main/java/SG-Evolution/client/HintProviderClient.java
@@ -1,46 +1,46 @@
-package lc.client;
+package SGE.client;
 
-import lc.LCRuntime;
-import lc.api.audio.ISoundController;
-import lc.api.defs.IContainerDefinition;
-import lc.api.defs.IRecipeDefinition;
-import lc.api.rendering.IParticleMachine;
-import lc.blocks.BlockBrazier;
-import lc.blocks.BlockConfigurator;
-import lc.blocks.BlockDHD;
-import lc.blocks.BlockLanteaDoor;
-import lc.blocks.BlockObelisk;
-import lc.client.openal.ClientSoundController;
-import lc.client.opengl.ParticleMachine;
-import lc.client.render.fabs.blocks.BlockBrazierRenderer;
-import lc.client.render.fabs.blocks.BlockConfiguratorRenderer;
-import lc.client.render.fabs.blocks.BlockDHDRenderer;
-import lc.client.render.fabs.blocks.BlockDoorRenderer;
-import lc.client.render.fabs.blocks.BlockObeliskRenderer;
-import lc.client.render.fabs.entities.EntityStaffProjectileRenderer;
-import lc.client.render.fabs.items.ItemDecoratorRenderer;
-import lc.client.render.fabs.tiles.TileConfiguratorRenderer;
-import lc.client.render.fabs.tiles.TileDHDRenderer;
-import lc.client.render.fabs.tiles.TileDoorRenderer;
-import lc.client.render.fabs.tiles.TileStargateBaseRenderer;
-import lc.client.render.fabs.tiles.TileTransportRingRenderer;
-import lc.common.LCLog;
-import lc.common.base.LCBlock;
-import lc.common.base.LCItem;
-import lc.common.base.LCTile;
-import lc.common.base.pipeline.LCBlockRenderPipeline;
-import lc.common.base.pipeline.LCEntityRenderPipeline;
-import lc.common.base.pipeline.LCItemRenderPipeline;
-import lc.common.base.pipeline.LCTileRenderPipeline;
-import lc.common.impl.registry.DefinitionRegistry;
-import lc.entity.EntityStaffProjectile;
-import lc.items.ItemDecorator;
-import lc.server.HintProviderServer;
-import lc.tiles.TileConfigurator;
-import lc.tiles.TileDHD;
-import lc.tiles.TileLanteaDoor;
-import lc.tiles.TileStargateBase;
-import lc.tiles.TileTransportRing;
+import SGE.SGERuntime;
+import SGE.api.audio.ISoundController;
+import SGE.api.defs.IContainerDefinition;
+import SGE.api.defs.IRecipeDefinition;
+import SGE.api.rendering.IParticleMachine;
+import SGE.blocks.BlockBrazier;
+import SGE.blocks.BlockConfigurator;
+import SGE.blocks.BlockDHD;
+import SGE.blocks.BlockLanteaDoor;
+import SGE.blocks.BlockObelisk;
+import SGE.client.openal.ClientSoundController;
+import SGE.client.opengl.ParticleMachine;
+import SGE.client.render.fabs.blocks.BlockBrazierRenderer;
+import SGE.client.render.fabs.blocks.BlockConfiguratorRenderer;
+import SGE.client.render.fabs.blocks.BlockDHDRenderer;
+import SGE.client.render.fabs.blocks.BlockDoorRenderer;
+import SGE.client.render.fabs.blocks.BlockObeliskRenderer;
+import SGE.client.render.fabs.entities.EntityStaffProjectileRenderer;
+import SGE.client.render.fabs.items.ItemDecoratorRenderer;
+import SGE.client.render.fabs.tiles.TileConfiguratorRenderer;
+import SGE.client.render.fabs.tiles.TileDHDRenderer;
+import SGE.client.render.fabs.tiles.TileDoorRenderer;
+import SGE.client.render.fabs.tiles.TileStargateBaseRenderer;
+import SGE.client.render.fabs.tiles.TileTransportRingRenderer;
+import SGE.common.LCLog;
+import SGE.common.base.LCBlock;
+import SGE.common.base.LCItem;
+import SGE.common.base.LCTile;
+import SGE.common.base.pipeline.LCBlockRenderPipeline;
+import SGE.common.base.pipeline.LCEntityRenderPipeline;
+import SGE.common.base.pipeline.LCItemRenderPipeline;
+import SGE.common.base.pipeline.LCTileRenderPipeline;
+import SGE.common.impl.registry.DefinitionRegistry;
+import SGE.entity.EntityStaffProjectile;
+import SGE.items.ItemDecorator;
+import SGE.server.HintProviderServer;
+import SGE.tiles.TileConfigurator;
+import SGE.tiles.TileDHD;
+import SGE.tiles.TileLanteaDoor;
+import SGE.tiles.TileStargateBase;
+import SGE.tiles.TileTransportRing;
 import net.minecraft.entity.Entity;
 import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
@@ -49,15 +49,15 @@ import net.minecraftforge.fml.client.registry.RenderingRegistry;
 /**
  * Client-side hint provider implementation
  * 
- * @author AfterLifeLochie
+ * @author Exodus Games
  * 
  */
 public class HintProviderClient extends HintProviderServer {
 
-	private LCBlockRenderPipeline blockRenderingHook;
-	private LCTileRenderPipeline tileRenderingHook;
-	private LCItemRenderPipeline itemRenderingHook;
-	private LCEntityRenderPipeline entityRenderingHook;
+	private SGEBlockRenderPipeline blockRenderingHook;
+	private SGETileRenderPipeline tileRenderingHook;
+	private SGEItemRenderPipeline itemRenderingHook;
+	private SGEEntityRenderPipeline entityRenderingHook;
 
 	private ClientSoundController soundController;
 	private ParticleMachine particleMachine;
@@ -71,11 +71,11 @@ public class HintProviderClient extends HintProviderServer {
 	@Override
 	public void preInit() {
 		super.preInit();
-		blockRenderingHook = new LCBlockRenderPipeline(
+		blockRenderingHook = new SGEBlockRenderPipeline(
 				RenderingRegistry.getNextAvailableRenderId());
-		tileRenderingHook = new LCTileRenderPipeline();
-		itemRenderingHook = new LCItemRenderPipeline();
-		entityRenderingHook = new LCEntityRenderPipeline();
+		tileRenderingHook = new SGETileRenderPipeline();
+		itemRenderingHook = new SGEItemRenderPipeline();
+		entityRenderingHook = new SGEEntityRenderPipeline();
 		soundController = new ClientSoundController();
 		particleMachine = new ParticleMachine();
 		RenderingRegistry.registerBlockHandler(
@@ -124,7 +124,7 @@ public class HintProviderClient extends HintProviderServer {
 	public void provideHints(IContainerDefinition definition) {
 		super.provideHints(definition);
 		if (definition.getBlock() != null) {
-			LCBlock theBlock = (LCBlock) definition.getBlock();
+			SGEBlock theBlock = (SGEBlock) definition.getBlock();
 			theBlock.setRenderer(blockRenderingHook.getRenderId());
 		}
 
