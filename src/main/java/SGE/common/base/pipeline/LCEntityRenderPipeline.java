a5bc4914de2f4733386d1712e17663abfbfe01da
DrakeBD <DrakeBD@users.noreply.github.com>
2018-11-30 15:57:03 +0000

    Update LCEntityRenderPipeline.java

--- src/main/java/SGE/common/base/pipeline/LCEntityRenderPipeline.java
+++ src/main/java/SGE/common/base/pipeline/LCEntityRenderPipeline.java
@@ -1,20 +1,20 @@
-package lc.common.base.pipeline;
+package SGE.common.base.pipeline;
 
-import lc.LCRuntime;
-import lc.api.defs.ILanteaCraftRenderer;
-import lc.common.LCLog;
-import lc.common.base.LCEntityRenderer;
-import lc.common.impl.registry.DefinitionRegistry;
-import lc.common.impl.registry.DefinitionRegistry.RendererType;
+import SGE.SGERuntime;
+import SGE.api.defs.ISGEvolutionRenderer;
+import SGE.common.SGELog;
+import SGE.common.base.SGEEntityRenderer;
+import SGE.common.impl.registry.DefinitionRegistry;
+import SGE.common.impl.registry.DefinitionRegistry.RendererType;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 
-public class LCEntityRenderPipeline extends Render {
+public class SGEEntityRenderPipeline extends Render {
 	private final DefinitionRegistry registry;
 
-	public LCEntityRenderPipeline() {
-		registry = (DefinitionRegistry) LCRuntime.runtime.registries().definitions();
+	public SGEEntityRenderPipeline() {
+		registry = (DefinitionRegistry) SGERuntime.runtime.registries().definitions();
 	}
 
 	public void useEntityTexture(Entity e) {
@@ -24,32 +24,31 @@ public class LCEntityRenderPipeline extends Render {
 	@Override
 	public void doRender(Entity e, double rpx, double rpy, double rpz, float yaw, float frame) {
 		boolean flag = true;
-		ILanteaCraftRenderer worker = registry.getRendererFor(RendererType.ENTITY, e.getClass());
-		if (worker == null || !(worker instanceof LCEntityRenderer))
+		ISGEvolutionRenderer worker = registry.getRendererFor(RendererType.ENTITY, e.getClass());
+		if (worker == null || !(worker instanceof SGEEntityRenderer))
 			flag = false;
 		else {
-			LCEntityRenderer entityRenderer = (LCEntityRenderer) worker;
+			SGEEntityRenderer entityRenderer = (SGEEntityRenderer) worker;
 			while (entityRenderer != null && !entityRenderer.doRender(this, e, rpx, rpy, rpz, yaw, frame)) {
 				worker = registry.getRenderer(RendererType.ENTITY, entityRenderer.getParent());
-				if (worker == null || !(worker instanceof LCEntityRenderer)) {
+				if (worker == null || !(worker instanceof SGEEntityRenderer)) {
 					flag = false;
 					break;
 				}
-				entityRenderer = (LCEntityRenderer) worker;
+				entityRenderer = (SGEEntityRenderer) worker;
 			}
 		}
 
 		if (!flag)
-			LCLog.warn("Unable to render entity class %s.", e.getClass());
-
+			SGELog.warn("Unable to render entity class %s.", e.getClass());
 	}
 
 	@Override
 	protected ResourceLocation getEntityTexture(Entity e) {
-		ILanteaCraftRenderer worker = registry.getRendererFor(RendererType.ENTITY, e.getClass());
-		if (worker == null || !(worker instanceof LCEntityRenderer))
+		ISGEvolutionRenderer worker = registry.getRendererFor(RendererType.ENTITY, e.getClass());
+		if (worker == null || !(worker instanceof SGEEntityRenderer))
 			return null;
-		LCEntityRenderer entityRenderer = (LCEntityRenderer) worker;
+		SGEEntityRenderer entityRenderer = (SGEEntityRenderer) worker;
 		return entityRenderer.getEntityTexture(this, e);
 	}
 
