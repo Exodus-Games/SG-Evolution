f117d7accee6362870e36998ad296326cd7e7e8d
DrakeBD <DrakeBD@users.noreply.github.com>
2018-11-30 14:07:49 +0000

    Update IRegistryContainer.java

--- src/main/java/SGE/api/components/IRegistryContainer.java
+++ src/main/java/SGE/api/components/IRegistryContainer.java
@@ -1,13 +1,13 @@
 /**
- * This file is part of the official LanteaCraft API. Please see the usage guide and
+ * This file is part of the official SG Evolution API. Please see the usage guide and
  * restrictions on use in the package-info file.
  */
-package lc.api.components;
+package SGE.api.components;
 
 /**
  * Registry container interface
  *
- * @author AfterLifeLochie
+ * @author Exodus Games
  *
  */
 public interface IRegistryContainer {
