af8dc2eaf4478fad4882b28a1341b2f63d6c4a26
DrakeBD <DrakeBD@users.noreply.github.com>
2018-11-30 14:53:49 +0000

    Update StreamingSoundFileConnection.java

--- src/main/java/SGE/client/openal/io/StreamingSoundFileConnection.java
+++ src/main/java/SGE/client/openal/io/StreamingSoundFileConnection.java
@@ -1,10 +1,10 @@
-package lc.client.openal.io;
+package SGE.client.openal.io;
 
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 
-import lc.common.LCLog;
+import SGE.common.SGELog;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.IResource;
 import net.minecraft.util.ResourceLocation;
@@ -13,7 +13,7 @@ import net.minecraft.util.ResourceLocation;
  * Streaming sound on-disk file connection between IResource and the OpenAL
  * driver.
  * 
- * @author AfterLifeLochie
+ * @author Exodus Games
  *
  */
 public class StreamingSoundFileConnection extends URLConnection {
@@ -35,14 +35,14 @@ public class StreamingSoundFileConnection extends URLConnection {
 	@Override
 	public InputStream getInputStream() {
 		try {
-			LCLog.debug("Looking for resource: %s", resourceName.toString());
+			SGELog.debug("Looking for resource: %s", resourceName.toString());
 			IResource rsrc = Minecraft.getMinecraft().getResourceManager().getResource(resourceName);
 			InputStream stream = rsrc.getInputStream();
 			if (stream == null)
 				throw new NullPointerException("stream == null!");
 			return stream;
 		} catch (Exception ex) {
-			LCLog.warn("Can't open sound stream", ex);
+			SGELog.warn("Can't open sound stream", ex);
 			return null;
 		}
 	}
