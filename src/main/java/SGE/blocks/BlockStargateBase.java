2e656f0d2c0a22057b20c014e98e5c280ab2ef85
DrakeBD <DrakeBD@users.noreply.github.com>
2018-11-30 14:41:14 +0000

    Update BlockStargateBase.java

--- src/main/java/SGE/blocks/BlockStargateBase.java
+++ src/main/java/SGE/blocks/BlockStargateBase.java
@@ -2,19 +2,19 @@ package SGE.blocks;
 
 import java.util.List;
 
-import lc.LCRuntime;
-import lc.LanteaCraft;
-import lc.api.components.ComponentType;
-import lc.api.defs.Definition;
-import lc.api.rendering.IBlockRenderInfo;
-import lc.api.stargate.StargateType;
-import lc.common.base.LCBlock;
-import lc.common.base.LCTile;
-import lc.common.base.multiblock.MultiblockState;
-import lc.common.configuration.xml.ComponentConfig;
-import lc.common.resource.ResourceAccess;
-import lc.items.ItemBlockStargateBase;
-import lc.tiles.TileStargateBase;
+import SGE.SGERuntime;
+import SGE.SGEvolution;
+import SGE.api.components.ComponentType;
+import SGE.api.defs.Definition;
+import SGE.api.rendering.IBlockRenderInfo;
+import SGE.api.stargate.StargateType;
+import SGE.common.base.SGEBlock;
+import SGE.common.base.SGETile;
+import SGE.common.base.multiblock.MultiblockState;
+import SGE.common.configuration.xml.ComponentConfig;
+import SGE.common.resource.ResourceAccess;
+import SGE.items.ItemBlockStargateBase;
+import SGE.tiles.TileStargateBase;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
@@ -138,7 +138,7 @@ public class BlockStargateBase extends SGEBlock {
 			float cy, float cz) {
 		TileStargateBase te = (TileStargateBase) world.getTileEntity(x, y, z);
 		if (te != null && te.getState() == MultiblockState.FORMED) {
-			player.openGui(LanteaCraft.instance, LCRuntime.runtime.interfaces().stargateUI.getGUIID(), world, x, y, z);
+			player.openGui(SGEvolution.instance, SGERuntime.runtime.interfaces().stargateUI.getGUIID(), world, x, y, z);
 			return true;
 		}
 		return false;
