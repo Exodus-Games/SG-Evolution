278db22f281deb24896b3c4695d5bc2fa8de5517
DrakeBD <DrakeBD@users.noreply.github.com>
2018-11-30 15:11:28 +0000

    Update GFXFluidDroplet.java

--- src/main/java/SGE/client/render/gfx/particle/GFXFluidDroplet.java
+++ src/main/java/SGE/client/render/gfx/particle/GFXFluidDroplet.java
@@ -1,6 +1,6 @@
 package SGE.client.render.gfx.particle;
 
-import SGE.client.opengl.LCEntityFX;
+import SGE.client.opengl.SGEEntityFX;
 import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.material.Material;
 import net.minecraft.util.MathHelper;
