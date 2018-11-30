76e0dc5a9279e58dec8cd83ee4b3b8e74b930105
DrakeBD <DrakeBD@users.noreply.github.com>
2018-11-30 14:36:20 +0000

    Update package-info.java

--- src/main/java/SGE/api/package-info.java
+++ src/main/java/SGE/api/package-info.java
@@ -1,35 +1,35 @@
 /**
  * <p>
- * This file is part of the official LanteaCraft API.
+ * This file is part of the official SG Evolution API.
  * </p>
  *
  * <p>
- * You are hereby permitted to do as you wish with the LanteaCraft API, so long as it is
+ * You are hereby permitted to do as you wish with the SG Evolution API, so long as it is
  * redistributed unmodified and in full only. You may upload it in source-form with your
  * projects or interact with it in any way you feel is appropriate.
  * </p>
  *
  * <p>
- * We accept absolutely no liability and offer no warranty; the LanteaCraft API is provided on
- * an as-is basis. We won't and can't be held liable if the LanteaCraft API doesn't work
+ * We accept absolutely no liability and offer no warranty; the SG Evolution API is provided on
+ * an as-is basis. We won't and can't be held liable if the SG Evolution API doesn't work
  * properly or breaks things. We also reserve the right to change the LanteaCraft API as
  * needed, and we do not guarantee our changes will be back-ported or backward compatible.
  * </p>
  *
  * <p>
- * Please do not package the LanteaCraft API in binary form with your distributions. If the
- * LanteaCraft API is changed - which may occur at any time - your binary distribution of the
- * LanteaCraft API can and most likely will conflict with those distributed internally by
- * LanteaCraft.
+ * Please do not package the SG Evolution API in binary form with your distributions. If the
+ * SG Evolution API is changed - which may occur at any time - your binary distribution of the
+ * SG Evolution API can and most likely will conflict with those distributed internally by
+ * SG Evolution.
  * </p>
  *
  * <p>
  * If you think we have made an error or you would like (limited) advice on how to implement
- * the LanteaCraft API, you can contact us at #LanteaCraft @ irc.esper.net. Good luck!
+ * the SG Evolution API, you can contact us at Exodus Games. Good luck!
  * </p>
  *
- * @since LanteaCraft RC 1.0
- * @author AfterLifeLochie
+ * @since SG Evolution RC 1.0
+ * @author Exodus Games
  */
-package lc.api;
+package SGE.api;
 
