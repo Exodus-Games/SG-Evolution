79b99bc7c7563be2120dc447892cfbdb254fbea8
DrakeBD <DrakeBD@users.noreply.github.com>
2018-11-30 14:29:37 +0000

    Update IBlockSkinnable.java

--- src/main/java/SGE/api/rendering/IBlockSkinnable.java
+++ src/main/java/SGE/api/rendering/IBlockSkinnable.java
@@ -1,8 +1,8 @@
 /**
- * This file is part of the official LanteaCraft API. Please see the usage guide and
+ * This file is part of the official SG Evolution API. Please see the usage guide and
  * restrictions on use in the package-info file.
  */
-package lc.api.rendering;
+package SGE.api.rendering;
 
 import net.minecraft.block.Block;
 
@@ -10,7 +10,7 @@ import net.minecraft.block.Block;
  * Contract interface for tile entities which are capable of being 'painted'
  * with other blocks.
  *
- * @author AfterLifeLochie
+ * @author Exodus Games
  *
  */
 public interface IBlockSkinnable {
